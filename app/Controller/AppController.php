d2496ae95ec47895d676c392b10fca65ee79e528
kengofly <root@ip-172-31-27-164.ap-northeast-1.compute.internal>
2015-05-15 03:09:18 +0000

    vol/01 complete

--- app/Controller/AppController.php
+++ app/Controller/AppController.php
@@ -21,6 +21,7 @@
 
 App::uses('Controller', 'Controller');
 
+
 /**
  * Application Controller
  *
@@ -31,5 +32,9 @@ App::uses('Controller', 'Controller');
  * @link		http://book.cakephp.org/2.0/en/controllers.html#the-app-controller
  */
 class AppController extends Controller {
-    public $components = array('DebugKit.Toolbar');
+	//public $components = array('DebugKit.Toolbar');
+	public $components = array(
+	'DebugKit.Toolbar',
+	'RequestHandler'
+);
 }
