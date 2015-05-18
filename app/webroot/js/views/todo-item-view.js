8a308ff7a1d75b4b6004f7d05f2eaa3d41db1229
kengofly <study@ip-172-31-27-164.ap-northeast-1.compute.internal>
2015-05-18 11:38:10 +0000

    vol/02 lesson5 complete

--- app/webroot/js/views/todo-item-view.js
+++ app/webroot/js/views/todo-item-view.js
@@ -8,12 +8,21 @@ var app = app || {};
 		
 		//テンプレート
 		template : _.template($('#item-template').html()),
-		
+	
+		//DOMイベントハンドラ設定
+		events : {
+			//チェックボックスクリック時
+			'click.toggle' : 'onStatusToggleClick',
+		},
+
 		initialize : function() {
 		},
 		render : function(){
 			this.$el.html(this.template(this.model.toJSON()));
 			return this;
+		},
+		onStatusToggleClick : function(e) {
+			this.model.toggle();
 		},
 	});
 })(app);
